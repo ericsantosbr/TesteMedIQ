541986051b549d9f9e60c39b9ff9fd6b428aef38
Eric Santos <ericsantosbr2@gmail.com>
2024-10-16 10:28:46 -0300

    Postgres database configs adequations on compose

--- dataset/init.sql
+++ dataset/init.sql
@@ -141,7 +141,8 @@ CREATE TABLE "MedIQ".groups (
     updated_at timestamp without time zone NOT NULL,
     created_at timestamp without time zone NOT NULL,
     creator_id integer NOT NULL,
-    is_active boolean
+    is_active boolean DEFAULT true NOT NULL,
+    description character varying
 );
 
 
@@ -342,6 +343,7 @@ COPY "MedIQ".discussion_posts (id, message, created_at, updated_at, user_id, pos
 11	Resposta aos amiguinhos!	2024-10-11 01:42:30.186	2024-10-11 01:42:30.186	9	5	t
 13	Resposta dada, amiguinhos!!! [redacted]	2024-10-11 01:42:38.955	2024-10-11 05:12:32.984	9	5	t
 14	Resposta aos amiguinhos! Vai dar tudo certo!	2024-10-11 04:11:40.857	2024-10-11 06:00:55.5	9	5	f
+15	Discordo	2024-10-16 13:17:49.631	2024-10-16 13:17:49.631	9	7	t
 \.
 
 
@@ -352,6 +354,7 @@ COPY "MedIQ".discussion_posts (id, message, created_at, updated_at, user_id, pos
 COPY "MedIQ".discussions (id, title, created_at, updated_at, message, owner_id, group_id, is_active) FROM stdin;
 6	título	2024-10-11 06:03:52.212	2024-10-11 06:09:08.192	Post do moderador aqui amigos	36	1	f
 5	título	2024-10-11 01:30:53.707	2024-10-14 14:38:48.23	boa tarde amigos	9	1	t
+7	título	2024-10-16 13:17:15.777	2024-10-16 13:17:15.777	Post do user aqui amigos	38	5	t
 \.
 
 
@@ -359,8 +362,12 @@ COPY "MedIQ".discussions (id, title, created_at, updated_at, message, owner_id, 
 -- Data for Name: groups; Type: TABLE DATA; Schema: MedIQ; Owner: postgres
 --
 
-COPY "MedIQ".groups (id, name, updated_at, created_at, creator_id, is_active) FROM stdin;
-1	Hematologia	2024-10-10 23:36:22.046	2024-10-10 23:36:22.046	9	\N
+COPY "MedIQ".groups (id, name, updated_at, created_at, creator_id, is_active, description) FROM stdin;
+1	Hematologia	2024-10-10 23:36:22.046	2024-10-10 23:36:22.046	9	t	\N
+2	Pediatria	2024-10-16 13:12:39.464	2024-10-16 13:12:39.464	37	t	\N
+3	Pediatria	2024-10-16 13:13:23.355	2024-10-16 13:13:23.355	37	t	\N
+4	Pediatria	2024-10-16 13:13:42.089	2024-10-16 13:13:42.089	37	t	\N
+5	Dermatologia	2024-10-16 13:14:39.039	2024-10-16 13:14:39.039	37	t	\N
 \.
 
 
@@ -373,7 +380,6 @@ COPY "MedIQ".reactions (id, user_id, reaction, post_id, reacted_at) FROM stdin;
 7	2	LIKE	11	2024-10-15 13:03:18.812154
 8	36	HAHA	11	2024-10-15 13:03:18.812154
 9	37	DISLIKE	11	2024-10-15 13:03:18.812154
-10	3	LIKE	11	2024-10-15 13:05:30.020319
 \.
 
 
@@ -389,6 +395,7 @@ COPY "MedIQ".users (id, email, username, password, created_at, updated_at, is_ac
 37	admin@user	admin	$2b$12$9FZkM5c7BbO0HFXn0FL9.emCbRIk8qF6c0Fol7jAJiH6tPGY5eMGy	2024-10-11 13:35:59.058	2024-10-11 13:35:59.058	t	ADMIN
 1	eric@santos	ericsantos	1234	2024-10-10 01:05:00	2024-10-11 13:37:07.07	t	USER
 3	erricc@santos	erricc	ericc	2024-10-10 01:05:00	2024-10-11 13:38:55.119	t	USER
+38	novo@usuario	novousuario	$2b$12$nZKz7il2afZMtvEZidIEzOM8/kQO1gQe6K8HrfMjo6Wvp2FJOMggS	2024-10-16 00:42:52.586	2024-10-16 00:42:52.586	t	USER
 \.
 
 
@@ -396,7 +403,7 @@ COPY "MedIQ".users (id, email, username, password, created_at, updated_at, is_ac
 -- Name: discussion_posts_id_seq; Type: SEQUENCE SET; Schema: MedIQ; Owner: postgres
 --
 
-SELECT pg_catalog.setval('"MedIQ".discussion_posts_id_seq', 14, true);
+SELECT pg_catalog.setval('"MedIQ".discussion_posts_id_seq', 15, true);
 
 
 --
@@ -417,14 +424,14 @@ SELECT pg_catalog.setval('"MedIQ".discussion_posts_user_id_seq', 1, false);
 -- Name: groups_id_seq; Type: SEQUENCE SET; Schema: MedIQ; Owner: postgres
 --
 
-SELECT pg_catalog.setval('"MedIQ".groups_id_seq', 1, true);
+SELECT pg_catalog.setval('"MedIQ".groups_id_seq', 5, true);
 
 
 --
 -- Name: newtable_id_seq; Type: SEQUENCE SET; Schema: MedIQ; Owner: postgres
 --
 
-SELECT pg_catalog.setval('"MedIQ".newtable_id_seq', 6, true);
+SELECT pg_catalog.setval('"MedIQ".newtable_id_seq', 7, true);
 
 
 --
@@ -438,14 +445,14 @@ SELECT pg_catalog.setval('"MedIQ".newtable_owner_id_seq', 1, false);
 -- Name: reactions_id_seq; Type: SEQUENCE SET; Schema: MedIQ; Owner: postgres
 --
 
-SELECT pg_catalog.setval('"MedIQ".reactions_id_seq', 10, true);
+SELECT pg_catalog.setval('"MedIQ".reactions_id_seq', 31, true);
 
 
 --
 -- Name: users_id_seq; Type: SEQUENCE SET; Schema: MedIQ; Owner: postgres
 --
 
-SELECT pg_catalog.setval('"MedIQ".users_id_seq', 37, true);
+SELECT pg_catalog.setval('"MedIQ".users_id_seq', 38, true);
 
 
 --
@@ -478,6 +485,14 @@ ALTER TABLE ONLY "MedIQ".discussions
 
 ALTER TABLE ONLY "MedIQ".reactions
     ADD CONSTRAINT reactions_pk PRIMARY KEY (id);
+
+
+--
+-- Name: reactions reactions_unique; Type: CONSTRAINT; Schema: MedIQ; Owner: postgres
+--
+
+ALTER TABLE ONLY "MedIQ".reactions
+    ADD CONSTRAINT reactions_unique UNIQUE (user_id, post_id);
 
 
 --
